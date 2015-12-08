df278eb48aa9e93a964e0de83945f0aeec7f583b
Morgan Stallings <morgan.stallings@emc.com>
2015-12-08 15:12:04 -0500

    Initial Commit

---
+++ src/test/java/com/madwithinsanity/IngestionApplicationTests.java
@@ -0,0 +1,16 @@
+package com.madwithinsanity;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = IngestionApplication.class)
+public class IngestionApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
